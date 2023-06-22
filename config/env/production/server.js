ff4ea0720fe350337c7c35d97b3df9d853719c10
asuman <asumanwebdev@gmailcom>
2023-06-22 10:17:48 +0300

    'commit'

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,8 @@
-module.exports = ({ env }) => ({
- proxy: true,
- url: env('MY_HEROKU_URL'),
- app: {
-  keys: env.array('APP_KEYS')
- },
-})
+// module.exports = ({ env }) => ({
+//  proxy: true,
+//  url: env('MY_HEROKU_URL'),
+//  app: {
+//   keys: env.array('APP_KEYS')
+//  },
+// })
 
